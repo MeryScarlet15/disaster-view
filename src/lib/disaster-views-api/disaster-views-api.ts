722ab56e828967f9d15d73eba052cfa281bcbcfb
MeryScarlet15 <meryscarlet15@gmail.com>
2022-09-30 21:24:59 +0200

    test vercel environment variables

--- src/lib/disaster-views-api/disaster-views-api.ts
+++ src/lib/disaster-views-api/disaster-views-api.ts
@@ -2,9 +2,12 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 import { earthquakesDisasterApi } from "./earthquakes/earthquake-requests"
 
 const client = new ApolloClient({
-  uri: process.env.DISASTER_VIEW_API_URL,
+  uri: process.env.API_URL,
   cache: new InMemoryCache()
 })
+
+console.log('...',process.env.API_URL)
+
 
 export default {
   earthquakes: earthquakesDisasterApi(client)
